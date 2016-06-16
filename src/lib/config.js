99c4ae0373395cd900e2c174553a5138d03a4835
Jacob Cox <jcox@C02QW5VDG8WP.local>
2016-06-16 16:37:53 -0600

    Adding compiled folder to the test path.

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,6 +2,6 @@
  * Created by jcox on 6/16/16.
  */
 export const browserDefaults = {width: 1024, height: 768, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:36.0) Gecko/20100101 Firefox/36.0'};
-export const defaultTestPath = '../../../tests/e2e/';
+export const defaultTestPath = '../../../tests/e2e/compiled/';
 export const defaultStartUrl = 'https://www.google.com/';
 
